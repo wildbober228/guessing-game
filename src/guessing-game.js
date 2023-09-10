e21a335fcd610f4ae0262d440b0241bcf400fb0b
wildbober228 <irina20000@tut.by>
2023-09-10 18:00:20 +0300

    init: init task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    low;
+    great;
+    result;
+    constructor() {
+        this.great = 0;
+        this.low = 0;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.great = max;
+        this.low = min;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.great - this.low) / 2) + this.low;
+        return this.result;
     }
 
     lower() {
-
+        this.great = this.result;
     }
 
     greater() {
-
+        this.low = this.result;
     }
 }
 
